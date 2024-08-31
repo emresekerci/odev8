82e40dcb6da821047716f25a2a34edb784503bf1
Emre Şekerci <153073499+emresekerci@users.noreply.github.com>
2024-08-31 19:06:59 +0300

    Add files via upload

---
+++ odev8.sql
@@ -0,0 +1,30 @@
+-- update employee
+-- set name = 'Emre Şekerci'
+-- where id = 3;
+
+
+-- update employee
+-- set email = 'emresekerci@windowslive.com'
+-- where name = 'Emre Şekerci';
+
+-- UPDATE EMPLOYEE
+-- SET EMAIL = NULL
+-- WHERE BIRTHDAY >= '2005-01-01';
+
+-- UPDATE EMPLOYEE 
+-- SET BIRTHDAY = '1989-01-06'
+-- WHERE NAME = 'Emre Şekerci';
+
+ -- update employee
+ -- set name = 'John johansson'
+ -- where id = 6;
+ 
+ -- delete from employee
+ -- where id = 2;
+ 
+ -- delete from employee
+ -- where name like 'n&';
+
+ -- delete from employee
+ -- where birthday > '2003-01-01'
+ 
